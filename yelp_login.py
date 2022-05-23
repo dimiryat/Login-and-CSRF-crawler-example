b7adb09b7f08b3d79b9d7c10cb5d108f670cd086
Dennis Lin <dennis.dlin2@gmail.com>
2022-05-23 13:17:26 +0800

    Add files via upload

---
+++ yelp_login.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 20 17:12:23 2022
+
+@author: DennisLin
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+s= requests.session()
+resp = s.get("https://www.yelp.com/login")
+soup = BeautifulSoup(resp.text, 'html5lib')
+csrf = soup.find('form', id = 'ajax-login').find('input', 'csrftok')['value']
+print(csrf)
+
+form_data = {
+    'email' : 'dennis.dlin2@gmail.com',
+    'password' : 'rTULL1F3yelp',
+    'csrftok' : csrf
+    }
+
+resp = s.post("https://www.yelp.com/login", data = form_data)
+print(resp.text)
+#print('The Front Porch' in resp.text)
